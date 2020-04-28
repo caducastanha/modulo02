e277ce0ff0b11e69de65799e07ee048797785302
Cadu Castanha <caducastanha@gmail.com>
2020-04-28 18:58:53 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cd697e7a906771',
+    pass: 'bf9bc6448705a3',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
